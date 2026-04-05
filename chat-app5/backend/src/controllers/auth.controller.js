84a57a6bbccdf4141a813af756aa7b35de291d58
SHAHRIL MAD SHAH <popiang@hotmail.com>
2026-04-06 02:57:09 +0800

    asdf

---
+++ chat-app5/backend/src/controllers/auth.controller.js
@@ -0,0 +1,115 @@
+import { generateToken } from "../libs/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import cloudinary from "../libs/cloudinary.js";
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+
+    try {
+        if (password.length < 6) {
+            return res
+                .status(400)
+                .json({ message: "Password must be at least 6 characters" });
+        }
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword,
+        });
+
+        if (newUser) {
+            await newUser.save();
+            generateToken(newUser._id, res);
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
+    } catch (error) {
+        console.log("Error in signup controller method: ", error.message);
+        res.status(400).json({ message: "Internal server error" });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res
+                .status(400)
+                .json({ message: "Invalid username or password" });
+        }
+
+        generateToken(user._id, res);
+        res.status(201).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller method: ", error.message);
+        res.status(400).json({ message: "Internal Server Error" });
+    }
+};
+
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Successfully logged out" });
+    } catch (error) {
+        console.log("Error in logout controller method: ", error.message);
+        res.status(400).json({ message: "Internal Server Error" });
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body;
+        const userId = req.user._id;
+
+        if (!profilePic) {
+            return res
+                .status(400)
+                .json({ message: "Profile picture is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+        const updateUser = await User.findByIdAndUpdate(
+            userId,
+            {
+                profilePic: uploadResponse.secure_url,
+            },
+            { new: true },
+        );
+    } catch (error) {
+        console.log("Error in updateProfile controller method: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
